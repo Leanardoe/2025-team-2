c082d8c9ad816c7c4a70ad1f63f46bbed5231943
Mike Obs <michael.r.obernesser@gmail.com>
2025-04-03 23:35:57 -0400

    Sorry again for the delay

--- ResumeSystem/Models/Database/Skill.cs
+++ ResumeSystem/Models/Database/Skill.cs
@@ -1,9 +1,13 @@
-﻿namespace ResumeSystem.Models.Database
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResumeSystem.Models.Database
 {
     public class Skill
     {
-        //TODO add data annotation
+        [Key]
         public int SkillID { get; set; }
+
+        [Required(ErrorMessage = "A candidate ID is required for a skill.")]
         public int CandidateID { get; set; }
 
         public Candidate Candidate { get; set; }
