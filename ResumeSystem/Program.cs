ce1410cd189a2a5db6c2a327b7b92a440efa1ab7
ryan.leanardoe@gmail.com <rlemons@morriscreative.com>
2025-04-16 08:56:04 -0400

    program cs

--- ResumeSystem/Program.cs
+++ ResumeSystem/Program.cs
@@ -1,5 +1,4 @@
 using OpenAI;
-
 using Microsoft.EntityFrameworkCore;
 using ResumeSystem.Models.Database;
 
@@ -7,16 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Enable session services
+builder.Services.AddDistributedMemoryCache(); // Required for session
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// Add OpenAI DI
 builder.Services.AddSingleton<OpenAIClient>(_ =>
 {
     var apiKey = builder.Configuration["OpenAI:ApiKey"];
     return new OpenAIClient(apiKey);
 });
 
-
-
-//Add EF Core DI
-builder.Services.AddDbContext<ResumeContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ResumeContext")));
+// Add EF Core DI
+builder.Services.AddDbContext<ResumeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ResumeContext")));
 
 var app = builder.Build();
 
@@ -24,7 +33,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -32,6 +40,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
